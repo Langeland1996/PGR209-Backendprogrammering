c8217ffa7518b36a7ef16353a6e33e3cb5de369a
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-09-28 13:17:56 +0200

    ⁵ Jetty serves files from disk

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Books with authors:
 * [ ] Add a books
 * [ ] List books
 
-* [ ] Maven project
+* [x] Maven project
 * [ ] Servlet API
-  * [ ] Jetty embedded in main 
+  * [x] Jetty embedded in main 
   
-* [ ] Serving files from disk
+* [x] Serving files from disk
 * [ ] Implement POST business logic with servlet
 * [ ] Implement GET business logic with servlet
 * [ ] View data with Java Server Pages (JSP)
--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,8 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +12,13 @@ public class LibraryServer {
     private final Server server = new Server(9080);
 
     public void start() throws Exception {
+        server.setHandler(new WebAppContext(
+                Resource.newClassPathResource("/webapp"),
+                "/"
+        ));
+
         server.start();
-        logger.warn("Starter servcer on {}", server.getURI());
+        logger.info("Starter servcer on {}", server.getURI());
     }
 
     public static void main(String[] args) throws Exception {
