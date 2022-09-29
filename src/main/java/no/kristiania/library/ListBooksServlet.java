8b8d369ddb74f8162620980c0a63bf8cb3406866
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-09-29 11:14:20 +0200

    ⁹ LostBooksServlets lists all the books in the books repository.

---
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -0,0 +1,31 @@
+package no.kristiania.library;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.servlet.Source;
+
+import java.io.IOException;
+
+public class ListBooksServlet extends HttpServlet {
+    private final BookRepository bookRepository;
+
+    public ListBooksServlet(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("<html>");
+        resp.getWriter().write("Here are the books in the category"+ req.getPathInfo());
+        resp.getWriter().write("<ul>");
+        for (Book book : bookRepository.listAll()){
+            resp.getWriter().write("<li>"+book+"</li>");
+        }
+        resp.getWriter().write("</ul>");
+        resp.getWriter().write("</html>");
+
+    }
+}
