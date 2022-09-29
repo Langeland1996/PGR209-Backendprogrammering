30abd6e08d42b483595e6371ff911a5f6a278a45
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-09-29 10:13:35 +0200

    ⁷ Handle add book

--- src/main/java/no/kristiania/library/AddBookServlet.java
+++ src/main/java/no/kristiania/library/AddBookServlet.java
@@ -6,13 +6,23 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.servlet.Source;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class AddBookServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(AddBookServlet.class);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Book book = new Book();
+        book.setTitle(req.getParameter("bookTitle"));
+        book.setAuthor(req.getParameter("bookAuthor"));
+
+        logger.info("Adding book {}", book);
+
+
 
     }
 }
