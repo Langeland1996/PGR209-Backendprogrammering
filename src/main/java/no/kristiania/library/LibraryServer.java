f4c8f7b91ae68cd5b72f6e61e637e442a72f24a8
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-10-18 12:34:12 +0200

    Commited to checkout of branch

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -3,36 +3,42 @@ package no.kristiania.library;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class LibraryServer {
-
+    private static final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
     private final Server server;
 
     public LibraryServer(int port) {
 
         this.server = new Server(port);
 
-        // context itself
+        // content itself
         WebAppContext webContext = new WebAppContext();
-        // what path the context should be placed
+        // what path the content should be placed
         webContext.setContextPath("/");
-        // where we get the context from
+        // where we get the content from
         webContext.setBaseResource(Resource.newClassPathResource("/webapp"));
-        // deliver context
+        // deliver content
         server.setHandler(webContext);
     }
 
     public URL getURL() throws MalformedURLException {
-        System.out.println("\n===============================================");
-        System.out.println("Server live URL --> " + server.getURI().toURL());
-        System.out.println("===============================================");
         return server.getURI().toURL();
     }
 
     public void start() throws Exception {
+        server.start();
+        logger.info("Server starting at {}", this.getURL());
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        var server = new LibraryServer(8080);
         server.start();
 
     }
